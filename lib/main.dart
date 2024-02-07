51b787d9dcdb93acb41db08466ce80876d6c50f0
alhasann351 <alhasann352@gmail.com>
2024-02-07 07:08:32 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'myhomepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(6, 57, 112, 1),
+        textTheme: GoogleFonts.amaranthTextTheme(),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
